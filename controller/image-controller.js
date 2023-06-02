5d1af6b6394c2d9d55d762f9a2cb3d5eec56eb61
CSEStudentAbhi <abhishekambi2003@gmail.com>
2023-06-02 21:08:18 +0530

    new modify

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -12,7 +12,7 @@ export const uploadImage = async (request, response) => {
     
     try {
         const file = await File.create(fileObj);
-        response.status(200).json({ path: `http://localhost:${process.env.PORT}/file/${file._id}`});
+        response.status(200).json({ path: `http://https://file-upload-m81u.onrender.com/file/${file._id}`});
     } catch (error) {
         console.error(error.message);
         response.status(500).json({ error: error.message });
